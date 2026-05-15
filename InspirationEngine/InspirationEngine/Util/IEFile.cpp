004193532b34f3c329389ef35eae2e614ddd6d26
Xom18 <ksg941109@naver.com>
2026-05-16 00:27:06 +0900

    IEFile vector로 변경

--- InspirationEngine/InspirationEngine/Util/IEFile.cpp
+++ InspirationEngine/InspirationEngine/Util/IEFile.cpp
@@ -1,4 +1,4 @@
-﻿#include <fstream>
+#include <fstream>
 #include "Core/MacroDefine.h"
 #include "IEFile.h"
 
@@ -14,26 +14,24 @@ IEFile::~IEFile()
 
 void IEFile::Reset()
 {
-	pKILL(m_data);
-	m_size = 0;
+	m_data.clear();
 	m_cursor = 0;
 }
 
 bool IEFile::ReadFile(const char* filename)
 {
 	Reset();
-	//파일을 읽어들인다, (파일명, 파일형식)
 	std::ifstream in(filename, std::ifstream::binary | std::ios::in);
 
-	//만약 들어올 내용이 없으면 false 반환
 	if (!in)
 		return false;
 
-	in.seekg(0, std::ifstream::end);//읽기 포인터의 위치를 파일의 맨 끝으로
-	m_size = in.tellg();//파일 시작위치와 현재위치 사이의 길이<자료형>(파일의 총 길이) 구하기
-	in.seekg(0, std::ifstream::beg);//다시 읽기 포인터를 맨 앞으로
-	m_data = new char[static_cast<int32_t>(m_size)];//버퍼에 새로운 공간을 할당한다
-	in.read(m_data, m_size);//읽는다
+	in.seekg(0, std::ifstream::end);
+	std::streamsize size = in.tellg();
+	in.seekg(0, std::ifstream::beg);
+
+	m_data.resize(static_cast<size_t>(size));
+	in.read(m_data.data(), size);
 
 	return true;
 }
