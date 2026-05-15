b55e9dd3f843e43698f71dc52ad10364b512616b
Xom18 <ksg941109@naver.com>
2026-05-16 00:27:56 +0900

    필요없어진 매크로 제거

--- InspirationEngine/InspirationEngine/Core/MacroDefine.h
+++ InspirationEngine/InspirationEngine/Core/MacroDefine.h
@@ -1,10 +1,5 @@
 #pragma once
 
-//어디 한곳에 놓기 애매한 매크로
-
-#define KILL(ptr) {if(ptr != nullptr) delete ptr; ptr = nullptr;}		//단일 삭제
-#define pKILL(ptr) {if(ptr != nullptr) delete[] ptr; ptr = nullptr;}	//배열 삭제
-
 /// <summary>
 /// 정수형 X, Y있는 변수
 /// </summary>
